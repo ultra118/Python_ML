1af50f2ddb8ee5d692c482be859c19067efa46ee
SungMin IM <ultra3484@gmail.com>
2019-04-15 21:11:40 +0900

    얼굴 특징 검출 sub process

---
+++ Kaggle/facial_keypoints/subprocess_test.py
@@ -0,0 +1,14 @@
+
+# coding: utf-8
+
+# In[5]:
+
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+print(5)
+
+a = pd.DataFrame([1,2,3])
+a.to_csv('C:/python_ML/Kaggle/facial_keypoints/bb.csv');
+
